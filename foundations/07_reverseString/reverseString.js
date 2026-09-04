c8ddf0e7db984502f4ffcfe4c3e09bda90bf11c7
Mavi Medeiros <144542240+mavimedeiros@users.noreply.github.com>
2026-09-04 11:46:20 -0300

    Complete reverseString exercise

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    return str.split("").reverse().join("");
 };
+
+reverseString("hello there");
 
 // Do not edit below this line
 module.exports = reverseString;
