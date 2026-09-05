e62aa99b1417c0448c26a22b01b1a05ba25449cd
Mavi Medeiros <144542240+mavimedeiros@users.noreply.github.com>
2026-09-05 13:12:12 -0300

    Complete leapYear exercise

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    const isDivisibleByFour = year % 4 === 0;
+    const isDivisibleByFourHundred = year % 400 === 0;
+    const isCentury = year % 100 === 0;
+    if (isDivisibleByFour && (!isCentury || isDivisibleByFourHundred)) {return true;}
+    else { return false }
 };
+
+leapYears(2000); // is a leap year: returns true
+leapYears(1985); // is not a leap year: returns false
 
 // Do not edit below this line
 module.exports = leapYears;
