20a44618e14c5ccf23fa7537db92ae9b66ec6889
Mavi Medeiros <144542240+mavimedeiros@users.noreply.github.com>
2026-09-25 11:21:59 -0300

    Complete findTheOldest exercise

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,33 @@
-const findTheOldest = function() {
+// Find person with greatest age
+// Find the oldest person if yearOfDeath is undefined. The person is alive to this day so use current year date.
+// Find the oldest person if yearOfDeath is undefined for the oldest person. Oldest person found is still alive.
+// If year of death is undefined define current year as to find their age.
 
+const getAge = function(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+};
+
+// const findTheOldest = function(people) {
+//     return people.sort(function(a, b) {
+//         const firstPerson = getAge(a.yearOfDeath, a.yearOfBirth);
+//         const nextPerson = getAge(b.yearOfDeath, b.yearOfBirth);
+//         return firstPerson < nextPerson ? b : a;
+//     });
+// };
+
+
+const findTheOldest = function (people) {
+  return people.reduce((oldest, currentPerson) => {
+    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    const currentAge = getAge(
+      currentPerson.yearOfBirth,
+      currentPerson.yearOfDeath
+    );
+    return oldestAge < currentAge ? currentPerson : oldest;
+  });
 };
 
 // Do not edit below this line
